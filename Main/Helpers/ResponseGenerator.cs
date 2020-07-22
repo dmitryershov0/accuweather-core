0f8a4002acebec1014506a5c813a5c35b91532ea
Dmitrii Ershov <dmitryershov0@gmail.com>
2020-07-22 16:53:51 +0300

    comments + fix empty respose message

--- Main/Helpers/ResponseGenerator.cs
+++ Main/Helpers/ResponseGenerator.cs
@@ -1,5 +1,3 @@
-using System.Text;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Accuweather.Core.Models;
@@ -7,29 +5,42 @@ using System.Text.Json;
 
 namespace Accuweather.Core.Helpers
 {
-    public static class ResponseGenerator
-    {
-        public static async Task<Response> GetResponse(HttpResponseMessage response)
-        {
-            var res = new Response
-            {
-                StatusCode = response.StatusCode
-            };
+	/// <summary>
+	/// Generate response
+	/// </summary>
+	public static class ResponseGenerator
+	{
+		/// <summary>
+		/// Returns custom response object.
+		/// </summary>
+		/// <param name="response">Api respose.</param>
+		/// <returns></returns>
+		public static async Task<Response> GetResponse(HttpResponseMessage response)
+		{
+			var res = new Response
+			{
+				StatusCode = response.StatusCode
+			};
 
-            if (!response.IsSuccessStatusCode)
-            {
-                res.Message = response.ReasonPhrase;
-                return res;
-            }
-            res.Data = await response.Content.ReadAsStringAsync();
-            
-            return res;
-        }
+			if (!response.IsSuccessStatusCode)
+			{
+				res.Message = response.ReasonPhrase;
+				return res;
+			}
+			res.Data = await response.Content.ReadAsStringAsync();
 
-        public static async Task<string> GetResponseJson(HttpResponseMessage response)
-        {
-           var res = await ResponseGenerator.GetResponse(response);
-           return JsonSerializer.Serialize(res);
-        }
-    }
+			return res;
+		}
+
+		/// <summary>
+		/// Returns custom response json.
+		/// </summary>
+		/// <param name="response">Api response.</param>
+		/// <returns></returns>
+		public static async Task<string> GetResponseJson(HttpResponseMessage response)
+		{
+			var res = await ResponseGenerator.GetResponse(response);
+			return JsonSerializer.Serialize(res);
+		}
+	}
 }
--- Main/Models/Response.cs
+++ Main/Models/Response.cs
@@ -1,9 +1,28 @@
 using System.Net;
 
 namespace Accuweather.Core.Models{
+
+    /// <summary>
+    /// Custom response
+    /// </summary>
     public class Response{
+        
+        /// <summary>
+        /// Status code
+        /// </summary>
+        /// <value></value>
         public HttpStatusCode StatusCode {get;set;}
+
+        /// <summary>
+        /// Message
+        /// </summary>
+        /// <value></value>
         public string Message {get;set;}
+
+        /// <summary>
+        /// Response Accuweather Api
+        /// </summary>
+        /// <value></value>
         public string Data {get;set;}
 
     }
